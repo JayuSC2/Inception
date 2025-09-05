1b786529aecc71e0c51aae9451b8da990b5eeb5b
Jayu <jayuze@gmail.com>
2025-09-05 17:50:18 +0200

    need to figure out login on wordpress page

--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Define the WordPress path
 WP_PATH="/var/www/html"
 
 echo "Waiting for MariaDB to be ready..."
@@ -16,10 +15,8 @@ done
 if ! wp core is-installed --path="$WP_PATH" --allow-root; then
     echo "WordPress not found. Installing..."
 
-    # Download WordPress core files
     wp core download --path="$WP_PATH" --allow-root
 
-    # Create wp-config.php
     wp config create --path="$WP_PATH" \
         --dbname="$MYSQL_DATABASE" \
         --dbuser="$MYSQL_USER" \
@@ -27,7 +24,6 @@ if ! wp core is-installed --path="$WP_PATH" --allow-root; then
         --dbhost="mariadb" \
         --allow-root
 
-    # Install WordPress
     wp core install --path="$WP_PATH" \
         --url="$DOMAIN_NAME" \
         --title="My WordPress Site" \
@@ -36,7 +32,6 @@ if ! wp core is-installed --path="$WP_PATH" --allow-root; then
         --admin_email="$WP_ADMIN_EMAIL" \
         --allow-root
 
-    # Create a non-admin user
     wp user create "$WP_USER" "$WP_USER_EMAIL" \
         --role=author \
         --user_pass="$WP_USER_PASSWORD" \
